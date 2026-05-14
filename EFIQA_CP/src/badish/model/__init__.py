b78ca72d95178ebae24477e1f4f2a8097e0cd9fb
MICCAI2026-3819 <miccai2026@yahoo.com>
2026-05-14 15:42:30 +0200

    Delete EFIQA_CP directory

--- EFIQA_CP/src/badish/model/__init__.py
+++
@@ -1,5 +0,0 @@
-import pkgutil
-import importlib
-
-for _, module_name, _ in pkgutil.walk_packages(__path__):
-    importlib.import_module(f"{__name__}.{module_name}")
--- EFIQA_CP/src/vuad/loss/basic_losses.py
+++
@@ -1,6 +0,0 @@
-from vuad.utils.registry import LossRegistry
-from torch import nn
-
-LossRegistry.register(name="mse")(nn.MSELoss)
-LossRegistry.register(name="l1")(nn.L1Loss)
-
